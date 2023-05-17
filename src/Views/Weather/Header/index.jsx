a2d0a4c75325da6f13ed1a699a82300f038f3d15
Luis Miguel TM <ok.rm1011@gmail.com>
2023-05-16 22:16:27 -0500

    add search

--- src/Views/Weather/Header/index.jsx
+++ src/Views/Weather/Header/index.jsx
@@ -1,29 +1,59 @@
-import { faGrip, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faGrip, faSearch, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 const Header = () => {
+  const [isActiveSearch, setIsActiveSearch] = useState(false);
+
+  const handleClickSearch = () => {
+    console.log("gaaaaaa");
+  };
   return (
     <div className="flex justify-around items-center">
-      {/* <form>
-        <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
-          Search
-        </label>
-        <div className="relative">
-          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <FontAwesomeIcon icon={faSearch} size="lg" />
+      {isActiveSearch && (
+        <>
+          <div
+            className="relative 
+                      flex 
+                      items-center 
+                      w-10/12"
+          >
+            <FontAwesomeIcon
+              onClick={handleClickSearch}
+              icon={faSearch}
+              size="lg"
+              className="absolute left-4"
+            />
+            <input
+              type="email"
+              className="px-12 
+                        py-3 
+                        rounded-full
+                        border-2
+                        w-full
+                        border-[#3b6396]"
+            />
+            <FontAwesomeIcon
+              onClick={() => setIsActiveSearch(false)}
+              icon={faXmark}
+              size="lg"
+              className="absolute right-4 cursor-pointer"
+            />
           </div>
-          <input
-            type="search"
-            id="default-search"
-            className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search Mockups, Logos..."
-            required
+        </>
+      )}
+      {!isActiveSearch && (
+        <>
+          <FontAwesomeIcon
+            onClick={() => setIsActiveSearch(true)}
+            icon={faSearch}
+            size="lg"
+            className="cursor-pointer"
           />
-        </div>
-      </form> */}
-      <FontAwesomeIcon icon={faSearch} size="lg" />
-      <p className="text-lg font-bold text-[#3b6396]">City/Country</p>
-      <FontAwesomeIcon icon={faGrip} size="lg" />
+          <p className="text-lg font-bold text-[#3b6396]">City/Country</p>
+          <FontAwesomeIcon icon={faGrip} size="lg" />
+        </>
+      )}
     </div>
   );
 };
